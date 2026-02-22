9390f508f89c4d0e46ef21290838582cae46837d
Carlos Marquina <carlosscripto@gmail.com>
2026-02-22 23:09:04 +0100

    function to get a bookmark object by userId and bookmarkId implemented

--- app.js
+++ app.js
@@ -4,37 +4,46 @@ const data = {
     "3" : [],    
     "4" : [],    
     "5" : [],    
-
 }
 
 
 //Create function to create each bookmarks
-
 function createBookmark (title , description , url) {
-    const bookmark = {
-        id : Date.now().toString(),
-        url : url,
-        title : title,
-        description : description,
-        createdAt : new Date().toISOString(),
-        likes : 0,
-    }
-    return bookmark;
+	const bookmark = {
+		id : Date.now().toString(),
+		url : url,
+		title : title,
+		description : description,
+		createdAt : new Date().toISOString(),
+		likes : 0,
+	}
+	return bookmark;
 }
 
 
 /* 
  * Implemment a function to push a bookmark object into a user array
  */
- 
 function pushBookmark (userId , bookmark) {
-   const array = data[userId]
-    console.log(array)
-    array.push(bookmark)
-    console.log(array)
+	const array = data[userId]
+	array.push(bookmark)
 }
 
-const bookmarkX = createBookmark("GOOGLE", "XXXXX", "URL");
 
-pushBookmark("2", bookmarkX)
-pushBookmark("2", bookmarkX)
+pushBookmark("2", createBookmark("GOOGLE", "GGGGGG", "GOOGLE.COM"))
+pushBookmark("2", createBookmark("INSTAGRAM", "IGIGIGIG", "IG.COM"))
+pushBookmark("2", createBookmark("YAHOO", "YYYYYY", "YAHOOO.COM"))
+
+/*
+ * Get bookmark by userId and bookmarkId
+ */
+function getBookmark(userId, bookmarkId) {
+	return data[userId].find(item => item.id === bookmarkId);
+}
+
+
+
+console.log(
+	getBookmark("2", data["2"][1].id)
+)
+
