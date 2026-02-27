c827ba1a7fb34cc77edf954d40dbd78671e65867
Carlos Marquina <carlosscripto@gmail.com>
2026-02-27 20:12:52 +0100

    Extract logic functions to bookmarks.js module

--- app.js
+++ app.js
@@ -34,6 +34,7 @@
 // }
 
 import { getUserIds, setData, getData } from './storage.js';
+import { uuidv4, createBookmark, pushBookmark, getBookmark } from './bookmarks.js';
 
 /*
  * Function to setup the data to localStorage
@@ -45,16 +46,6 @@ function setInitialData() {
 			setData(user, []);
 		}
 	});
-}
-
-/*
- * Source - https://stackoverflow.com/a/2117523
- * Funtion o generate a ramdon UUID
-*/
-function uuidv4() {
-  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-    (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
-  );
 }
 
 /**
@@ -106,55 +97,6 @@ function validateForm(title, description, url) {
 	}
 	return true;
 }
-
-/*
- * Create function to create each bookmarks
- */
-function createBookmark (title , description , url) {
-	const bookmark = {
-		id : uuidv4(),
-		url : url,
-		title : title,
-		description : description,
-		createdAt : new Date().toISOString(),
-		likes : 0,
-	}
-	return bookmark;
-}
-
-/* 
- * Implemment a function to push a bookmark object into a user array
- */
-function pushBookmark (userId , bookmark) {
-	const array = getData(userId);
-	array.push(bookmark);
-	setData(userId, array)
-}
-
-/*
- * function to get a bookmark by userId and bookmarkId
- */
-function getBookmark(userID, bookmarkId) {
-	const bookmarks = getData(userID);
-	return bookmarks.find(bookmark => bookmark.id === bookmarkId)
-}
-
-//todo -> we can remove this function becouse we don't need it for now
-/*
- * Function to get likes from a bookmark
- */
-// function getLikes(bookmark) {
-// 	return bookmark.likes;
-// }
-
-// todo -> we have to see this function in the future
-/*
- * this function hope to receive a bookmark object to increment the likes property 
- */
-// function incrementLike(bookmark) {
-// 	bookmark.likes++
-// 	setData()
-// }
 
 /**
  * Copies a given text value to the clipboard.
